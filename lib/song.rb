8fec543dd52a0c7ce1c38dd73026ab7a6c36a465
prodigysurge <boatinglife6@yahoo.com>
2021-01-22 13:03:35 -0500

    commit tree 76cef7bce44a227b36f45bfafe5c8a4ad0332924

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,5 @@
 class Song
+  
   attr_accessor :name, :artist_name
   @@all = []
 
@@ -10,4 +11,61 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|obj|obj.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ||
+    self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    row = filename
+    data = row.split(" - ")
+    artist_name = data[0]
+    song_name = data[1].gsub(".mp3", "")
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename (filename)
+    songFile = filename.split(/[-.]+/)
+    song_new = self.create_by_name(songFile[1].strip)
+    song_new.artist_name = songFile[0].strip
+    song_new
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+
+
+
 end
+
